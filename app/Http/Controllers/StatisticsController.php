a6757cd47b719aaa7ef7101a49e95cf860feec97
gcichy <g.cichy2001@gmail.com>
2024-01-09 23:57:03 +0100

    charts_v2

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\ParentCycleView;
 use App\Models\User;
+use App\Models\Work;
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -24,11 +26,23 @@ class StatisticsController extends Controller
         $users = User::select('id','employeeNo','role')->get();
 
         try {
-            $data = $this->getLineBarChart($request);
+
+            $data = $this->getLineBarChart($request, $users);
             $chart_data_1 = $data['line_bar_chart'];
             $filt_start_date_1 = $data['dates'][0];
             $filt_end_date_1 = $data['dates'][1];
-            $validation_err = $data['validation_err'];
+            $validation_err_1 = $data['validation_err'];
+            $chart_title_1 = $data['chart_title'];
+            $filt_users_1 = $data['user_id_array'];
+
+
+            $data_2 = $this->getBarChart($request);
+            $chart_data_2 = $data_2['line_bar_chart'];
+            $filt_start_date_2 = $data_2['dates'][0];
+            $filt_end_date_2 = $data_2['dates'][1];
+            $validation_err_2= $data_2['validation_err'];
+            $chart_title_2 = $data_2['chart_title'];
+
         } catch(Exception $e) {
             Log::channel('error')->error('Error filtering statistics: '.$e->getMessage(), [
                 'employeeNo' => $employee_no,
@@ -39,21 +53,28 @@ class StatisticsController extends Controller
 
 
         return view('statistics.stat_dashboard', [
-            'filt_items' => [],
             'user' => $request->user(),
             'users' => $users,
             'chart_data_1' => $chart_data_1,
             'status_err' => isset($status_err)? $status_err : null,
-            'filt_start_date_1' => $filt_start_date_1,
-            'filt_end_date_1' => $filt_end_date_1,
-            'validation_err' => $validation_err,
+            'chart_title_1' => isset($chart_title_1)? $chart_title_1 : null,
+            'filt_users_1' => isset($filt_users_1)? $filt_users_1 : null,
+            'filt_start_date_1' => isset($filt_start_date_1)? $filt_start_date_1 : null,
+            'filt_end_date_1' => isset($filt_end_date_1)? $filt_end_date_1 : null,
+            'validation_err_1' => isset($validation_err_1)? $validation_err_1 : null,
+            'chart_data_2' => $chart_data_2,
+            'chart_title_2' => isset($chart_title_2)? $chart_title_2 : null,
+            'filt_users_2' => isset($filt_users_2)? $filt_users_2 : null,
+            'filt_start_date_2' => isset($filt_start_date_2)? $filt_start_date_2 : null,
+            'filt_end_date_2' => isset($filt_end_date_2)? $filt_end_date_2 : null,
+            'validation_err_2' => isset($validation_err_2)? $validation_err_2 : null,
         ]);
     }
 
     /**
      * @throws Exception
      */
-    private function getLineBarChart($request): array
+    private function getBarChart($request): array
     {
         $default_end_date = date("Y-m-d");
         $default_start_date = date("Y-m-d", strtotime("-2 weeks", strtotime(date("Y-m-d"))));
@@ -66,8 +87,83 @@ class StatisticsController extends Controller
             $validation_err = "Data 'Praca od' musi być wcześniej niż data 'Praca do'";
         }
 
+        $chart_data = DB::select("
+                                       select d.work_date,
+                                               d.min_date,
+                                               work.weighted_productivity_average,
+                                               work.work_duration,
+                                               work.amount
+
+                                        from (
+                                                 select
+                                                     min(full_date) as min_date,
+                                                     concat(year,'-',month,'-(',min(day_of_month),'-',max(day_of_month),')') as work_date,
+                                                     cast(concat(year,week_of_year) as int) as week_year_id
+                                                 from dates
+                                                 where full_date between '2024-01-01' and '2024-01-15'
+                                                 group by year,week_of_year,month
+                                             ) d
+                                                 left join (
+                                            select
+                                                week_year_id,
+                                                sum(productivity * duration_minute) / week_duration_minute as weighted_productivity_average,
+                                                avg(week_duration_minute) / 60 as work_duration,
+                                                avg(week_amount) as amount
+                                            from (
+                                                     select
+                                                         cast(concat(year(start_time),week(start_time))as int) as week_year_id,
+                                                         sum(duration_minute) over (partition by cast(concat(year(start_time),week(start_time))as int)) week_duration_minute,
+                                                         sum(amount) over (partition by cast(concat(year(start_time),week(start_time))as int)) week_amount,
+                                                         wv.*
+                                                     from work_view wv
+                                                     where component_id = 11
+                                                 ) wv
+                                            group by week_year_id, week_duration_minute) work
+                                                           on work.week_year_id = d.week_year_id
+                                        order by d.min_date");
+
+        $chart_title = 'Produktywność pracy dla';
+        $chart_data = json_encode($chart_data);
+        return ['line_bar_chart' => $chart_data,
+            'dates' => [$start_date, $end_date],
+            'validation_err' => isset($validation_err)? $validation_err : null,
+            'chart_title' => $chart_title,
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getlineBarChart($request, Collection $users): array
+    {
+
+        if(!is_null($request->employees)) {
+            $user_where_clause = $request->employees;
+            $user_id_array = explode(',',$user_where_clause);
+            $employee_no_string = User::whereIn('id', $user_id_array)->select('employeeNo')->get();
+            $employee_no_string = collect($employee_no_string)->map(function (User $arr) { return $arr->employeeNo; })->toArray();
+            $employee_no_string = implode(',', $employee_no_string);
+            $chart_title = 'Praca '.$employee_no_string;
+        } else {
+            $user_where = collect($users)->map(function (User $arr) { return $arr->id; })->toArray();
+            $user_where_clause = implode(', ', $user_where);
+            $chart_title = 'Praca firmy ogółem';
+
+        }
+
+        $default_end_date = date("Y-m-d");
+        $default_start_date = date("Y-m-d", strtotime("-2 weeks", strtotime(date("Y-m-d"))));
+        $end_date = is_null($request->end_date_1)? $default_end_date: $request->end_date_1;
+        $start_date = is_null($request->start_date_1)? $default_start_date : $request->start_date_1;
+
+        if($end_date <= $start_date ) {
+            $end_date = $default_end_date;
+            $start_date = $default_start_date;
+            $validation_err = "Data 'Praca od' musi być wcześniej niż data 'Praca do'";
+        }
+
         $chart_data = DB::select("select d.work_date,
-                                               work.weighetd_productivity_average,
+                                               work.weighted_productivity_average,
                                                work_duration
                                         from (
                                                  select
@@ -77,25 +173,33 @@ class StatisticsController extends Controller
                                                  where full_date between '".$start_date."' and '".$end_date."'
                                              ) d
                                                  left join (
-                                            select
-                                                date_id,
-                                                sum(productivity * duration_minute) / day_duration_minute as weighetd_productivity_average,
-                                                avg(day_duration_minute) / 60 as work_duration
-                                            from (
-                                                     select
-                                                         date_format(start_time, '%Y%m%d') AS date_id,
-                                                         sum(duration_minute) over (partition by date(start_time) order by date(start_time)) day_duration_minute,
-                                                         wv.*
-                                                     from work_view wv) wv
-                                            group by date_id) work
-                                                           on work.date_id = d.date_id
-                                        order by d.date_id");
+                                                          select
+                                                                date_id,
+                                                                sum(productivity * duration_minute) / day_duration_minute as weighted_productivity_average,
+                                                                avg(day_duration_minute) / 60 as work_duration
+                                                          from (
+                                                             select
+                                                                 date_format(start_time, '%Y%m%d') AS date_id,
+                                                                 sum(duration_minute_per_user) over (partition by date(start_time) order by date(start_time)) day_duration_minute,
+                                                                 wv.productivity,
+                                                                 wu.duration_minute_per_user as duration_minute
+                                                              from work_view wv
+                                                                      join work_user wu
+                                                                           on wu.work_id = wv.work_id
+                                                                           and wu.user_id in (".$user_where_clause.")) wv
+                                                            group by date_id) work
+                                                                on work.date_id = d.date_id
+                                            order by d.date_id");
 
 
         $chart_data = json_encode($chart_data);
         return ['line_bar_chart' => $chart_data,
             'dates' => [$start_date, $end_date],
             'validation_err' => isset($validation_err)? $validation_err : null,
+            'chart_title' => $chart_title,
+            'user_id_array' => isset($user_id_array)? $user_id_array : null,
         ];
     }
+
+
 }
--- resources/views/statistics/stat_dashboard.blade.php
+++ resources/views/statistics/stat_dashboard.blade.php
@@ -6,16 +6,8 @@
             </p>
         </div>
     @endif
-    @if(isset($chart_data_1) and isset($users) and isset($filt_items))
+    @if(isset($chart_data_1) and isset($users))
         <script type="module">
-            $('#filter-btn').on('click',function() {
-                let filterGrid = $('#filters');
-                if(filterGrid.hasClass('hidden')) {
-                    filterGrid.removeClass('hidden');
-                } else {
-                    filterGrid.addClass('hidden');
-                }
-            });
             // Pass PHP data to JavaScript
             let chartData = @json($chart_data_1);
             chartData = JSON.parse(chartData);
@@ -34,7 +26,7 @@
                 for (let i = 0; i < chartData.length; i++) {
                     let entry = chartData[i];
                     data.addRow([entry.work_date,
-                        parseFloat(entry.weighetd_productivity_average),
+                        parseFloat(entry.weighted_productivity_average),
                         parseFloat(entry.work_duration),
                         null
                     ]);
@@ -60,7 +52,7 @@
                     },
                     vAxes: {
                         0: {
-                            title: 'Średnia produktywność',
+                            title: 'Średnia produktywność (%)',
                             titleTextStyle: {
                                 fontSize: 20,
                                 fontName: 'Arial',
@@ -97,41 +89,38 @@
                     // }
                 };
 
-                var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+                var chart = new google.visualization.LineChart(document.getElementById('chart-div'));
                 chart.draw(data, options);
             }
         </script>
-        @php
-            $name = "Praca firmy ogółem";
-        @endphp
-        <x-information-panel :viewName="$name">
-            <x-nav-button  id="filter-btn" class="on-select details bg-yellow-300 hover:bg-yellow-600 ml-2 xl:mr-4">
-                {{ __('Filtry') }}
-            </x-nav-button>
-        </x-information-panel>
+        <div class="mt-2 flex flex-col xl:flex-row border-gray-300 xl:bg-white justify-between items-center z-[50]">
+            <a class ='block w-full xl:w-1/3 py-4 pl-3 pr-4 bg-white border-blue-450 border-l-4 lg:border-l-8 text-md md:text-lg lg:text-2xl text-left font-medium text-gray-800  transition duration-150 ease-in-out'>
+                {{isset($chart_title_1)? $chart_title_1 :'Praca firmy'}}
+            </a>
+        </div>
         <form method="GET" action="{{ route('stastistics.index') }}" enctype="multipart/form-data">
             <div class="w-full my-4 flex justify-center">
-                <div id="filters" class="flex flex-row justify-start w-[70%] border-2 rounded-lg hidden">
-                    <dl class="grid grid-cols-2 bg-white text-left rounded-l-lg w-4/5">
-{{--                        <div class="col-span-1 flex flex-col justify-center h-full">--}}
-{{--                            <a class ='block px-2 text-xs md:text-sm font-medium bg-gray-800 text-center text-white'>--}}
-{{--                                Pracownicy--}}
-{{--                            </a>--}}
-{{--                            <div class="p-1">--}}
-{{--                                @php $unique_id = 'employees' @endphp--}}
-{{--                                <x-select-multiple :uniqueId="$unique_id" :placeholder="__('Pracownicy')">--}}
-{{--                                    <x-slot name="options">--}}
-{{--                                        @foreach($users as $u)--}}
-{{--                                            <option value="{{$u->id}}" {{(array_key_exists($unique_id,$filt_items) and in_array($u->id, $filt_items[$unique_id]))? 'selected' : ''}}>--}}
-{{--                                                {{$u->employeeNo}}--}}
-{{--                                            </option>--}}
-{{--                                        @endforeach--}}
-{{--                                    </x-slot>--}}
-{{--                                </x-select-multiple>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-                        <div class="col-span-1 flex flex-col justify-start">
+                <div id="filters" class="flex flex-row justify-start w-[80%] border-2 rounded-lg">
+                    <dl class="grid grid-cols-3 bg-white text-left rounded-l-lg w-4/5">
+                        <div class="col-span-1 flex flex-col justify-center h-full">
                             <a class ='block px-2 text-xs md:text-sm font-medium bg-gray-800 text-center text-white rounded-tl-lg'>
+                                Pracownicy
+                            </a>
+                            <div class="p-1">
+                                @php $unique_id = 'employees' @endphp
+                                <x-select-multiple :uniqueId="$unique_id" :placeholder="__('Pracownicy')">
+                                    <x-slot name="options">
+                                        @foreach($users as $u)
+                                            <option value="{{$u->id}}" {{(isset($filt_users_1) and is_array($filt_users_1) and in_array($u->id, $filt_users_1))? 'selected' : ''}}>
+                                                {{$u->employeeNo}}
+                                            </option>
+                                        @endforeach
+                                    </x-slot>
+                                </x-select-multiple>
+                            </div>
+                        </div>
+                        <div class="col-span-1 flex flex-col justify-start">
+                            <a class ='block px-2 text-xs md:text-sm font-medium bg-gray-800 text-center text-white'>
                                 Praca od
                             </a>
                             <div class="p-1 flex justify-center items-center h-full">
@@ -163,6 +152,136 @@
                 </div>
             </div>
         </form>
-        <div id="chart_div" class="w-full h-[500px]"></div>
+        <div id="chart-div" class="w-full h-[500px]"></div>
+    @endif
+    @if(isset($chart_data_2))
+        <script type="module">
+
+            // Pass PHP data to JavaScript
+            let chartData = @json($chart_data_2);
+            chartData = JSON.parse(chartData);
+            console.log(chartData);
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+
+                let data = new google.visualization.DataTable();
+                data.addColumn('string', 'Tydzień pracy');
+                data.addColumn('number', 'Średnia produktywność');
+                data.addColumn('number', 'Wyprodukowana ilość');
+
+                for (let i = 0; i < chartData.length; i++) {
+                    let entry = chartData[i];
+                    data.addRow([entry.work_date,
+                        parseFloat(entry.weighted_productivity_average),
+                        parseFloat(entry.amount)
+                    ]);
+                }
+                //var data = google.visualization.arrayToDataTable(chartData);
+
+                var options = {
+                    curveType: 'function',
+                    series: {
+                        0: { type: 'bars', targetAxisIndex: 0 }, // Line chart
+                        1: { type: 'bars', targetAxisIndex: 1, bar: { groupWidth: '50%' } }
+                    },
+                    hAxis: {
+                        title: 'Tydzień pracy',
+                        titleTextStyle: {
+                            fontSize: 20,
+                            fontName: 'Arial',
+                            italic: false
+                        },
+                        slantedText: true,
+                        slantedTextAngle: 45
+                    },
+                    vAxes: {
+                        0: {
+                            title: 'Średnia produktywność (%)',
+                            titleTextStyle: {
+                                fontSize: 20,
+                                fontName: 'Arial',
+                                italic: false
+                            },
+                            viewWindow: { min: 0 },
+                        },
+                        1: {
+                            title: 'Wyprodukowana Ilość (szt)',
+                            titleTextStyle: {
+                                fontSize: 20,
+                                fontName: 'Arial',
+                                italic: false
+                            },
+                            minValue: 0,
+                            gridlines: { count: 0 }
+                        } // Set the minimum value for the bar chart axis
+                    },
+                    legend: { position: 'bottom' },
+                    chartArea: {
+                        top: '5%',
+                        height: '67%'  // Adjust the width as needed
+                    }
+                    // vAxis: {
+                    //     title: 'Średnia produktywność',
+                    //     titleTextStyle: {
+                    //         fontSize: 20,
+                    //         fontName: 'Arial',
+                    //         italic: false
+                    //     },
+                    //     viewWindow: {
+                    //         min: 0  // set the minimum value to 0
+                    //     }
+                    // }
+                };
+
+                var chart = new google.visualization.LineChart(document.getElementById('chart-div-2'));
+                chart.draw(data, options);
+            }
+        </script>
+
+        <div class="mt-5 flex flex-col xl:flex-row border-gray-300 xl:bg-white justify-between items-center z-[50]">
+            <a class ='block w-full xl:w-1/3 py-4 pl-3 pr-4 bg-white border-blue-450 border-l-4 lg:border-l-8 text-md md:text-lg lg:text-2xl text-left font-medium text-gray-800  transition duration-150 ease-in-out'>
+                {{isset($chart_title_2)? $chart_title_2 :'Praca firmy'}}
+            </a>
+        </div>
+        <form method="GET" action="{{ route('stastistics.index') }}" enctype="multipart/form-data">
+            <div class="w-full my-4 flex justify-center">
+                <div id="filters-2" class="flex flex-row justify-start w-[80%] border-2 rounded-lg">
+                    <dl class="grid grid-cols-3 bg-white text-left rounded-l-lg w-4/5">
+                        <div class="col-span-1 flex flex-col justify-start">
+                            <a class ='block px-2 text-xs md:text-sm font-medium bg-gray-800 text-center text-white'>
+                                Praca od
+                            </a>
+                            <div class="p-1 flex justify-center items-center h-full">
+                                <div id="exp-start-time" class="relative w-full">
+                                    <input type="date" name="start_date_2" value="{{isset($filt_start_date_1)? $filt_start_date_1 : null}}"
+                                           class="exp-start-time p-2 xl:p-2.5 block w-full text-xs xl:text-sm text-gray-900 border-gray-300 focus:bg-blue-150 focus:ring-blue-450 rounded"
+                                           placeholder="Start" min="2023-01-01"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-1 flex flex-col justify-center border-r-2">
+                            <a class ='block px-2 text-xs md:text-sm font-medium bg-gray-800 text-center text-white'>
+                                Praca do
+                            </a>
+                            <div class="p-1 flex justify-center items-center h-full">
+                                <div id="exp-end-time" class="exp-end-time relative w-full">
+                                    <input name="end_date_2" type="date" value="{{isset($filt_end_date_1)? $filt_end_date_1 : null}}"
+                                           class="p-2 xl:p-2.5 block w-full text-xs xl:text-sm text-gray-900 border-gray-300 focus:bg-blue-150 focus:ring-blue-450 rounded"
+                                           placeholder="Koniec" min="2023-01-01"/>
+                                </div>
+                            </div>
+                        </div>
+                    </dl>
+                    <div class="flex flex-col justify-center items-center bg-white xl:border-r-2 rounded-r-lg w-1/5">
+                        <button type="submit" class ='w-[60%] xl:w-[40%] text-sm md:text-lg bg-gray-800 hover:bg-gray-600 font-medium text-center text-white rounded-lg'>
+                            Filtruj
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <div id="chart-div-2" class="w-full h-[500px]"></div>
     @endif
 </x-app-layout>
